a858f6120c5e7c697bb55714d4098afee4143b1b
jovicadev94 <jovica.misirlic@gmail.com>
2021-07-15 01:21:53 +0200

    Add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes
-from app.measurements import models
+from app.measurements import models, measurement_bp
+
+app.register_blueprint(measurement_bp)
